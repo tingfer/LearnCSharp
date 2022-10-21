0fa2af02881629289072e9dc0e26e8e23e0490e7
oliver <oliver.lin@Tm-robot.com>
2022-10-21 17:30:44 +0800

    Fluent Assertion

--- LearnCSharp/UnitTest.cs
+++ LearnCSharp/UnitTest.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using FluentAssertions.Execution;
+
 namespace LearnCSharp;
 
 public class Tests
@@ -11,12 +14,30 @@ public class Tests
         Assert.True(true);
     }
 
-    [Test]
+    //[Test]
     public void YieldReturnTest()
     {
         var yieldReturn = new YieldReturn();
         yieldReturn.Demo1();
         yieldReturn.Demo2();
         Assert.True(true);
+    }
+
+    [Test]
+    public void FluentAssertionTest()
+    {
+        const string actual = "ABCDEFGHI";
+        actual.Should().StartWith("AB").And.EndWith("HI").And.Contain("EF").And.HaveLength(9);
+
+        IEnumerable<int> numbers = new[] { 1, 2, 3 };
+
+        numbers.Should().OnlyContain(n => n > 0);
+        // numbers.Should().HaveCount(4, "because we thought we put four items in the collection");
+
+        using (new AssertionScope())
+        {
+            5.Should().Be(10);
+            "Actual".Should().Be("Expected");
+        }
     }
 }
